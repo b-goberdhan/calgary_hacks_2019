31bdb21f836d7a5e153e55f33a564e9e9ce97289
Brandon G <brandongob@gmail.com>
2019-02-16 16:39:13 -0700

    screenshots being taken every 3 seconds

--- src/components/LiveFeed/LiveFeed.js
+++ src/components/LiveFeed/LiveFeed.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import Webcam from 'react-webcam';
+import './LiveFeed.css'
 class LiveFeed extends Component {
-    render() {
+  constructor(props) {
+    super(props);
+    this.setTimer();
+    
+  }  
+
+  render() {
         return (
-          <div>
-            <Webcam/>
+          <div className="Container">
+            <Webcam 
+              ref={this.setWebCamRef}
+              screenshotFormat="image/jpeg" className="Feed" />
           </div>
         );
-      }
+    }
+  setTimer() {
+    this.timer = setInterval(() => {
+      this.getSnapshot();
+    }, 3000);
+  }
+  setWebCamRef = webcam => {
+    this.webcam = webcam;
+  };
+  getSnapshot() {
+    this.imgSrc = this.webcam.getScreenshot();
+    console.log(this.imgSrc);
+  }
+  stopTimer() {
+    clearInterval(this.timer);
+  }
 }
 
 export default LiveFeed;
