976ccaa52bbdaed7bb6aa60fdf93977891d77bdc
Komal Waseem <komal.waseem@ucalgary.ca>
2019-02-16 19:15:29 -0700

    separate links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import LiveFeed from './components/LiveFeed/LiveFeed';
 import Uploader from './components/Uploader/Uploader';
+
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route path="/uploader" component={Uploader} />
+        <Route path="/live" component={LiveFeed} />
+      </Switch>
+    </div>
+  </Router>
+)
+
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <LiveFeed/>
-        <Uploader/>
-      </div>
+      routing
     );
   }
 }
