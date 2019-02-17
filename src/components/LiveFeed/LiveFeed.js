b9e4f4d95c015c067b33a4df214e249f000e603f
Lawrence Nguyen <law.lnguyen@gmail.com>
2019-02-16 19:26:09 -0700

    Added the POST REST call with sample image

--- src/components/LiveFeed/LiveFeed.js
+++ src/components/LiveFeed/LiveFeed.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import Webcam from 'react-webcam';
 import './LiveFeed.css'
+import { request } from 'request';
+
 class LiveFeed extends Component {
   constructor(props) {
     super(props);
     this.setTimer();
-  }  
+
+    this.uriBase = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect';
+    this.request = require('request');;
+    this.subscriptionKey = '02a4dbfb233f42869ba5cc1ff089006d';
+
+    this.params = {
+      'returnFaceId': 'true',
+      'returnFaceLandmarks': 'false',
+      'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
+          'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
+    };
+    
+    this.options = {
+      uri: this.uriBase,
+      qs: this.params,
+      body: null,
+      headers: {
+          'Content-Type': 'application/octet-stream',
+          'Ocp-Apim-Subscription-Key' : this.subscriptionKey
+      }
+    };
+  }
 
   render() {
         return (
@@ -19,17 +42,31 @@ class LiveFeed extends Component {
   setTimer() {
     this.timer = setInterval(() => {
       this.getSnapshot();
-    }, 3000);
+    }, 8000);
   }
   setWebCamRef = webcam => {
     this.webcam = webcam;
   };
   getSnapshot() {
-    this.imgSrc = this.webcam.getScreenshot();
+    this.imageData = this.webcam.getScreenshot();
+    this.detectFace();
     console.log(this.imgSrc);
   }
   stopTimer() {
     clearInterval(this.timer);
+  }
+
+  detectFace() {
+    this.options.body = this.imageData;
+    this.request.post(this.options, (error, response, body) => {
+      if (error) {
+        console.log('Error: ', error);
+        return;
+      }
+      let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
+      console.log('JSON Response\n');
+      console.log(jsonResponse);
+      });
   }
 }
 
