2c2060426ffe655adc68fcf1ff48d1048f0da900
Konrad Wisniewski <konrad.m.wisniewski@gmail.com>
2019-02-16 22:29:39 -0700

    Added calls to MS

--- src/components/LiveFeed/LiveFeed.js
+++ src/components/LiveFeed/LiveFeed.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import Webcam from 'react-webcam';
 import './LiveFeed.css'
-import { request } from 'request';
-var base64Img = require('base64-img');
+
 
 class LiveFeed extends Component {
   constructor(props) {
@@ -10,8 +9,17 @@ class LiveFeed extends Component {
     this.setTimer();
 
     this.uriBase = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect';
+    this.uriBaseIdentify = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/identify';
+
+
+    this.imgSrc = 'https://i.imgur.com/gJpYe4G.jpg'
+    
+    this.threshold = 0.70;
+   
+    // this.imgSrc = 'https://www.childprotection.sa.gov.au/sites/default/files/styles/banner/public/addressing-child-safety_banner_edit.jpg'
     this.request = require('request');;
     this.subscriptionKey = '';
+    this.personGroupId = 'c47803da-576f-41d8-a28e-7659f1ff171c';
 
     this.params = {
       'returnFaceId': 'true',
@@ -19,13 +27,15 @@ class LiveFeed extends Component {
       'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
           'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
     };
+
+
     
     this.options = {
       uri: this.uriBase,
       qs: this.params,
       body: null,
       headers: {
-          'Content-Type': 'application/octet-stream',
+          'Content-Type': 'application/json',
           'Ocp-Apim-Subscription-Key' : this.subscriptionKey
       }
     };
@@ -58,8 +68,7 @@ class LiveFeed extends Component {
   }
 
   detectFace() {
-    var buf = new Buffer(this.imageData, 'base64');
-    this.options.body = buf;
+    this.options.body = '{"url": ' + '"' + this.imgSrc + '"}';
     this.request.post(this.options, (error, response, body) => {
       if (error) {
         console.log('Error: ', error);
@@ -68,7 +77,105 @@ class LiveFeed extends Component {
       let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
       console.log('JSON Response\n');
       console.log(jsonResponse);
-      });
+      let detectedFaces = JSON.parse(jsonResponse);
+
+      var faceIDs = []
+
+      for (var i = 0 ; i < detectedFaces.length; i++) 
+      {
+        console.log(detectedFaces[i].faceId);
+        faceIDs.push('"' + detectedFaces[i].faceId + '"');
+        console.log(faceIDs[i]);
+      }
+
+      // If we get faceIds, send it to microsoft for match
+      if(faceIDs.length > 0)
+      {
+        var OptionsFaceIdentify = {
+          uri: this.uriBaseIdentify,
+          body: null,
+          headers: {
+              'Content-Type': 'application/json',
+              'Ocp-Apim-Subscription-Key' : this.subscriptionKey
+          }
+        };
+        
+        //OptionsFaceIdentify.body = '{"personGroupId": ' + '"' + this.personGroupId + '", "faceIds": [ "527f39b5-24d2-4104-b25a-012b822e0130", "5c83b373-ef9c-4624-8bdf-77e8d24bd1fb" ], "maxNumOfCandidatesReturned": 1, "confidenceThreshold": 0.5 }';
+        OptionsFaceIdentify.body = '{"personGroupId": ' + '"' + this.personGroupId + '", "faceIds": [' + faceIDs + '], "maxNumOfCandidatesReturned": 1, "confidenceThreshold": 0.5}';
+
+        this.request.post(OptionsFaceIdentify, (error, response, body) => {
+          if (error) {
+            console.log('Error: ', error);
+            return;
+          }
+          let jsonResponse2 = JSON.stringify(JSON.parse(body), null, '  ');
+          console.log('JSON Response\n');
+          console.log(jsonResponse2);
+
+          let potentialMatches = JSON.parse(jsonResponse2);
+
+          var matchingFace = null;
+
+          for (var i = 0 ; i < potentialMatches.length; i++) 
+          {
+            console.log(potentialMatches[i].candidates[0]);
+            console.log(potentialMatches[i].candidates[0].confidence);
+            if (potentialMatches[i].candidates[0].confidence > this.threshold)
+            {
+              console.log(potentialMatches[i].candidates[0].personId + " has made been found");
+              matchingFace = potentialMatches[i].candidates[0].personId;
+              break;
+            }
+          }
+
+          // If we get a matchingFace, send it to microsoft
+          if(matchingFace !== null)
+          {
+            console.log("Finding data for " + matchingFace);
+
+            var GetGroupPersonParams = {
+              'personGroupId': this.personGroupId,
+              'personId': matchingFace
+            };
+
+           var uriBaseIdentify = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/persongroups/' + this.personGroupId + '/persons/' + matchingFace
+           var GetGroupPersonOptions = {
+              uri: uriBaseIdentify,
+              qs: GetGroupPersonParams,
+              body: null,
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Ocp-Apim-Subscription-Key' : this.subscriptionKey
+              }
+            };
+
+            this.request.get(GetGroupPersonOptions, (error, response, body) => {
+              if (error) {
+                console.log('Error: ', error);
+                return;
+              }
+              let jsonResponse3 = JSON.stringify(JSON.parse(body), null, '  ');
+              console.log('JSON Response\n');
+              console.log(jsonResponse3);
+
+              let match = JSON.parse(jsonResponse3);            
+              
+              console.log("### Match Found! ###\nName: " + match.name + "\nUser Data: " + match.userData  )
+            });
+          }
+        });
+// {
+//     "personGroupId": "c47803da-576f-41d8-a28e-7659f1ff171c",
+//     "faceIds": [
+//         "527f39b5-24d2-4104-b25a-012b822e0130",
+//         "5c83b373-ef9c-4624-8bdf-77e8d24bd1fb"
+//     ],
+//     "maxNumOfCandidatesReturned": 1,
+//     "confidenceThreshold": 0.5
+// }
+      }
+      
+    });
   }
 }
 
