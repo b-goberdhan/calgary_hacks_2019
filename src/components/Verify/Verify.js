1f8b0fc87cc21226ba007240a44eb38cc1cc1f72
Komal Waseem <komal.waseem@ucalgary.ca>
2019-02-16 23:13:45 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/components/Verify/Verify.js

--- src/components/Verify/Verify.js
+++ src/components/Verify/Verify.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
-import './Verify.css'
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+import './Verify.css';
 import logo from './test.jpg';
 
 class Verify extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          startDate: new Date()
+        };
+        this.handleChange = this.handleChange.bind(this);
+      }
+     
+      handleChange(date) {
+        this.setState({
+          startDate: date
+        });
+      }
+
     render () {
         return (
             <div className='container-fluid'>
@@ -11,8 +29,16 @@ class Verify extends Component {
             <div className='parag1'>You will be notified when the missing person is identified.</div>
             <div className='parag2'>Please enter your email so we can contact you.</div>
             <form className="email">
+                <div className='parag2'>Please enter your email so we can contact you.</div>
                 <input type="text" className="email" name="email" />
+                <div className="parag2">Please enter the missing persons full name</div>
+                <input type="text" className="email" name="name" />
+                <div className="parag2">Please enter the last time they were seen</div>
             </form>
+            <DatePicker className="date"
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+            />
             <button className='submit'>Thank You</button>
             </div>
         );
